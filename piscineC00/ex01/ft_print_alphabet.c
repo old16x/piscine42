7dc2a2451c98682470feb58b36478937f318aa3c
rad16x <138368019+rad16x@users.noreply.github.com>
2023-09-06 22:09:45 +0200

    improve 01

--- piscineC00/ex01/ft_print_alphabet.c
+++ piscineC00/ex01/ft_print_alphabet.c
@@ -14,5 +14,16 @@
 
 void	ft_print_alphabet(void)
 {
-	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+	char c;
+
+	c = 'a';
+	while (c <= 'z') {
+		write(1, &c, 1);
+		c++;
+	}
+}
+
+int main(void) {
+	ft_print_alphabet();
+	return 0;
 }

fcae047105eca66d022384d937e53f4dc8deda89
rad16x <138368019+rad16x@users.noreply.github.com>
2023-09-11 18:46:47 +0200

    fix ft_strcpy

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -10,10 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, const char *src)
 {
-	while (*src)
-		*dest++ = *src++;
-	*dest = '\0';
-	return (src);
+	unsigned int i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }

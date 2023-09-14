7b101c9d4df1cf77e35fcf085b54d2a1486191e9
rad16x <138368019+rad16x@users.noreply.github.com>
2023-09-14 08:03:46 +0200

    retry C03

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -14,10 +14,10 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	i = 0;
 	if (n == 0)
 		return (0);
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+	while (i < (n - 1) && s1[i] != '\0' && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -12,19 +12,22 @@
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	int		i;
-	int		j;
+	unsigned int i;
+	unsigned int j;
 
 	i = 0;
-	j = 0;
+	if (ft_strlen(to_find) == 0)
+		return ((char*)str);
 	while (str[i])
 	{
-		while (str[i + j] == to_find[j] && str[i + j])
-			j++;
-		if (to_find[j] == '\0')
-			return (str + i);
-		i++;
 		j = 0;
+		while (to_find[j] == str[i + j])
+		{
+			if (to_find[j + 1] == '\0')
+				return ((char*)str + i);
+			j++;
+		}
+		i++;
 	}
 	return (0);
 }

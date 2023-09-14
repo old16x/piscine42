2718cb6c22bdf407698fc1d51a4708a4dea7eb69
rad16x <138368019+rad16x@users.noreply.github.com>
2023-09-14 09:19:29 +0200

    final

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -15,9 +15,23 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (i < (n - 1) && s1[i] != '\0' && s1[i] == s2[i])
+	while (i < n && (*s1 || *s2))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - ((unsigned char)s2[i]));
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
 }
+
+/*
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = -1;
+	while (++i < n && (*s1 || *s2))
+		if (s1[i] != s2[i])
+			return (((char *)s1)[i] - ((char *)s2)[i]);
+	return (0);
+}*/

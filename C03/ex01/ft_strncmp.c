30235b7fb63fee1a1b8c7dd99e973bf6732bb1af
rad16x <138368019+rad16x@users.noreply.github.com>
2023-09-14 09:24:55 +0200

    final

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -10,28 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-	unsigned int	i;
 
-	i = 0;
-	while (i < n && (*s1 || *s2))
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - ((unsigned char)s2[i]));
-		i++;
-	}
-	return (0);
+#include <stdio.h>
+
+int ft_strncmp1(char *s1, char *s2, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < n && (*s1 || *s2))
+    {
+        if (s1[i] != s2[i])
+            return ((unsigned char)s1[i] - ((unsigned char)s2[i]));
+        i++;
+    }
+    return (0);
 }
 
-/*
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int ft_strncmp2(char *s1, char *s2, unsigned int n)
 {
-	unsigned int	i;
+    unsigned int i;
 
-	i = -1;
-	while (++i < n && (*s1 || *s2))
-		if (s1[i] != s2[i])
-			return (((char *)s1)[i] - ((char *)s2)[i]);
-	return (0);
-}*/
+    i = -1;
+    while (++i < n && (*s1 || *s2))
+        if (s1[i] != s2[i])
+            return (((char *)s1)[i] - ((char *)s2)[i]);
+    return (0);
+}
+
+int main() {
+    char s1[] = {128, '\0'}; // 0x80
+    char s2[] = {0, '\0'};   // 0x00
+
+    int result1 = ft_strncmp1(s1, s2, 1);
+    int result2 = ft_strncmp2(s1, s2, 1);
+
+    printf("Result from ft_strncmp1: %d\n", result1);
+    printf("Result from ft_strncmp2: %d\n", result2);
+
+    return 0;
+}
+
--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -31,3 +31,27 @@ char	*ft_strstr(char *str, char *to_find)
 	}
 	return (0);
 }
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	tmp;
+
+	if (*to_find == '\0')
+		return (str);
+	while (*str)
+	{
+		if (*str == *to_find)
+		{
+			i = -1;
+			tmp = 0;
+			while (to_find[++i] != '\0')
+				if (str[i] != to_find[i])
+					tmp = 1;
+			if (tmp == 0)
+				return (str);
+		}
+		str++;
+	}
+	return (0);
+}

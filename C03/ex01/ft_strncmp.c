c13d0784e0012b95e7888cc48143dd765c9cf18d
Antoine Radix <aradix@bess-f4r4s11.clusters.42paris.fr>
2023-09-14 22:31:57 +0200

    C03 100%

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,48 +6,20 @@
 /*   By: aradix <aradix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 18:11:32 by aradix            #+#    #+#             */
-/*   Updated: 2023/09/13 16:32:00 by aradix           ###   ########.fr       */
+/*   Updated: 2023/09/14 20:01:40 by aradix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <stdio.h>
-
-int ft_strncmp1(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i;
+	unsigned int	i;
 
-    i = 0;
-    while (i < n && (*s1 || *s2))
-    {
-        if (s1[i] != s2[i])
-            return ((unsigned char)s1[i] - ((unsigned char)s2[i]));
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
 }
-
-int ft_strncmp2(char *s1, char *s2, unsigned int n)
-{
-    unsigned int i;
-
-    i = -1;
-    while (++i < n && (*s1 || *s2))
-        if (s1[i] != s2[i])
-            return (((char *)s1)[i] - ((char *)s2)[i]);
-    return (0);
-}
-
-int main() {
-    char s1[] = {128, '\0'}; // 0x80
-    char s2[] = {0, '\0'};   // 0x00
-
-    int result1 = ft_strncmp1(s1, s2, 1);
-    int result2 = ft_strncmp2(s1, s2, 1);
-
-    printf("Result from ft_strncmp1: %d\n", result1);
-    printf("Result from ft_strncmp2: %d\n", result2);
-
-    return 0;
-}
-
--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,88 +6,28 @@
 /*   By: aradix <aradix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 21:13:40 by aradix            #+#    #+#             */
-/*   Updated: 2023/09/13 20:23:49 by aradix           ###   ########.fr       */
+/*   Updated: 2023/09/14 12:32:46 by aradix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	unsigned int i;
-	unsigned int j;
+	unsigned int	i;
+	unsigned int	j;
 
 	i = 0;
-	if (ft_strlen(to_find) == 0)
-		return ((char*)str);
+	if (!to_find || *to_find == '\0')
+		return (str);
 	while (str[i])
 	{
 		j = 0;
 		while (to_find[j] == str[i + j])
 		{
 			if (to_find[j + 1] == '\0')
-				return ((char*)str + i);
+				return (str + i);
 			j++;
 		}
 		i++;
 	}
 	return (0);
-}
-
-char	*ft_strstr(char *str, char *to_find)
-{
-	int	i;
-	int	tmp;
-
-	if (*to_find == '\0')
-		return (str);
-	while (*str)
-	{
-		if (*str == *to_find)
-		{
-			i = -1;
-			tmp = 0;
-			while (to_find[++i] != '\0')
-				if (str[i] != to_find[i])
-					tmp = 1;
-			if (tmp == 0)
-				return (str);
-		}
-		str++;
-	}
-	return (0);
-}
-
-char *ft_strstr(char *str, char *to_find)
-{
-    unsigned int i;
-    unsigned int j;
-
-    i = 0;
-    if (to_find[0] == '\0')
-        return (str);
-    while (str[i])
-    {
-        j = 0;
-        while (to_find[j] == str[i + j])
-        {
-            if (to_find[j + 1] == '\0')
-                return (str + i);
-            j++;
-        }
-        i++;
-    }
-    return (0);
-}
-
-
-int main() {
-    char str[] = "Hello, World!";
-    char to_find[] = "";
-
-    char *result1 = ft_strstr1(str, to_find);
-    char *result2 = ft_strstr2(str, to_find);
-
-    printf("Result from ft_strstr1: %s\n", result1);
-    printf("Result from ft_strstr2: %s\n", result2);
-
-    return 0;
 }
